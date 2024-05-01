8f3c0431a4cc7fe3cb2150846aa84249a9084f15
timthebouncer <he01367637us@gmail.com>
2024-05-01 17:52:34 +0800

    client 配置: tailwind / vite config

--- client/myapp/src/App.tsx
+++ client/myapp/src/App.tsx
@@ -1,36 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useMemo, useState} from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-  
+interface Todo {
+    id: number;
+    text: string;
+    isComplete: boolean;
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+let id = 1
+
+function App() {
+
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [inputText, setInputText] = useState<string>('');
+    const [filterType, setFilterType] = useState<number>(0);
+
+
+    const handleDeleteTodo = (id: number): void => {
+        const filteredTodos = todos.filter(todo => todo.id !== id);
+        setTodos(filteredTodos);
+    };
+
+    const handleClickTodoText = (id: number): void => {
+        const updatedTodos = todos.map(todo =>
+            todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo
+        );
+        setTodos(updatedTodos);
+    };
+
+    const handleFilter=(type: number)=>{
+        setFilterType(type)
+    }
+
+    const submit=async ()=>{
+
+        const response = await fetch("http://localhost:3000/api");
+        console.log(response,'response')
+        const html = await response.text(); // HTML string
+        console.log(html,'html')
+
+        // fetch('http://localhost:3000/',{ mode: 'no-cors'}).then((res)=>{
+        //     console.log(res,'rrr')
+        //
+        //     console.log(aaa,'aaa')
+        // }).catch(err=>{
+        //     console.log(err,'eeee')
+        // })
+        if (inputText.trim() !== '') {
+            const newTodo: Todo = {
+                id: id++,
+                text: inputText,
+                isComplete: false,
+            };
+            setTodos([...todos, newTodo]);
+            setInputText('');
+        }
+    }
+
+    const filteredData = useMemo(() => {
+        let updatedTodos: Todo[] = [];
+        if (filterType === 0) {
+            updatedTodos = todos;
+        } else if (filterType === 1) {
+            updatedTodos = todos.filter(todo => todo.isComplete);
+        } else if (filterType === 2) {
+            updatedTodos = todos.filter(todo => !todo.isComplete);
+        }
+        return updatedTodos
+    }, [filterType, todos]);
+
+
+    return (
+        <div className="border py-4 px-8 flex flex-col justify-between" style={{width: 500, height: 400, borderRadius: '3%'}}>
+            <div className="flex justify-between mb-2">
+                <input className="border rounded-3xl border-amber-200"
+                       value={inputText}
+                       onChange={(e)=>setInputText(e.target.value)}
+                />
+                <button className="btn form__submit-btn" type="submit"  onClick={submit}>Add</button>
+            </div>
+            <div style={{overflow: "scroll", overflowX: 'hidden', flex: 1, marginBottom: 8}}>
+                {
+                    filteredData.map((item)=>{
+                        return (
+                            <div className="item flex justify-between items-center py-2">
+                                <div className="content" onClick={()=>handleClickTodoText(item.id)}>{item.text}</div>
+                                <button onClick={()=>handleDeleteTodo(item.id)}>
+                                    {item.isComplete ? 'V':'X'}
+                                </button>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="flex justify-between">
+                <button
+                    className="btn filters__btn filters__btn--all"
+                    onClick={()=>handleFilter(0)}
+                >
+                    All
+                </button>
+                <button
+                    className="btn filters__btn filters__btn--complete"
+                    onClick={()=>handleFilter(1)}
+                >
+                    Complete
+                </button>
+                <button
+                    className="btn filters__btn filters__btn--incomplete"
+                    onClick={()=>handleFilter(2)}
+                >
+                    Incomplete
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default App
